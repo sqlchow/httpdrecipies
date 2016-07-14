cddeff931b25e137fc2591c83ec3fc87f30f2ce4
Gopalakrishnan Subramanian <sgk1980@gmail.com>
2016-07-14 14:52:13 +0530

    Testing reverse update to Manage Engine

--- restart_httpd.rb
+++ restart_httpd.rb
@@ -12,8 +12,9 @@ servicelatency=            @input.get("servicelatency")
 serviceexecutiontime=      @input.get("serviceexecutiontime")
 serviceduration=           @input.get("serviceduration")
 hostaddress=               @input.get("hostaddress")
+manageenginerequestid=      @input.get("MANAGE_ENGINE_REQUESTID")
 
-@log.info("restart_httpd was called host "+ hostname)
+@log.info("restart_httpd was called host "+ hostname +"Related incident Ticket Number : "+ manageenginerequestid )
 
 if servicestate == "CRITICAL"                                       #service goes ‘Down’
   response=@call.connector("ssh")                                   #calling ssh connector   
@@ -29,3 +30,35 @@ if servicestate == "CRITICAL"                                       #service goe
   result=response.get("result")
   @log.info("#{result.to_s}")
 end
+
+if response.exitcode == 0	# if the previous call succeeds then Update Manage Engine
+   response=@call.connector("manageengine_connector_name")    
+              .set("action","update-request")
+              .set("request-id",manageenginerequestid)
+              .set("requester","administrator")
+              .set("subject","Testing update service request")
+              .set("description","Specific descriptio")
+              .set("requesttemplate","Unable to browse")
+              .set("priority","Low")
+              .set("site","-")
+              .set("group","Network")
+              .set("technician","Flint Operator")
+              .set("level","Tier 3")
+              .set("status","Close")
+              .set("service",@service)
+              .timeout(10000)                                                 
+              .sync
+    
+    result=response.get("result")
+    @log.info("#{result.to_s}")
+
+	if response.exitcode == 0               # 0 is success.
+	  puts "success"
+	  # take action in case of success
+	else                                    # non zero means fail
+	  puts "fail"
+	  puts "Reason:" + response.message     # get the reason of failure
+	  ## Take action in case of failure
+	end
+end 
+
