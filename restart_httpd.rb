4472b9ec4a10a89c0c332d6f44cc4f17dd89ea40
Gopalakrishnan Subramanian <sgk1980@gmail.com>
2016-07-14 16:11:27 +0530

    Further debugging

--- restart_httpd.rb
+++ restart_httpd.rb
@@ -12,7 +12,7 @@ servicelatency=            @input.get("servicelatency")
 serviceexecutiontime=      @input.get("serviceexecutiontime")
 serviceduration=           @input.get("serviceduration")
 hostaddress=               @input.get("hostaddress")
-manageenginerequestid=      @input.get(MANAGE_ENGINE_REQUESTID)
+manageenginerequestid=      @input.get("MANAGE_ENGINE_REQUESTID")
 
 @log.info("restart_httpd was called host "+ hostname +"Related incident Ticket Number : "+ manageenginerequestid )
 
@@ -31,6 +31,7 @@ if servicestate == "CRITICAL"                                       #service goe
   @log.info("#{result.to_s}")
 end
 
+ @log.info(""+manageenginerequestid)
 #if response.exitcode == 0	# if the previous call succeeds then Update Manage Engine
    response=@call.connector("manageenginesdp")    
               .set("action","update-request")
