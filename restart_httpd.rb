ca557412b5561ad20d16023dcfd8ac3973db9ada
Gopalakrishnan Subramanian <sgk1980@gmail.com>
2016-07-14 16:04:24 +0530

    Just debugging

--- restart_httpd.rb
+++ restart_httpd.rb
@@ -12,7 +12,7 @@ servicelatency=            @input.get("servicelatency")
 serviceexecutiontime=      @input.get("serviceexecutiontime")
 serviceduration=           @input.get("serviceduration")
 hostaddress=               @input.get("hostaddress")
-manageenginerequestid=      @input.get("MANAGE_ENGINE_REQUESTID")
+manageenginerequestid=      @input.get(MANAGE_ENGINE_REQUESTID)
 
 @log.info("restart_httpd was called host "+ hostname +"Related incident Ticket Number : "+ manageenginerequestid )
 
