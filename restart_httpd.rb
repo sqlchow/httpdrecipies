d8844ad6c9b3190e4a8fe260bdd4e4dd51f01ece
Gopalakrishnan Subramanian <sgk1980@gmail.com>
2016-08-03 12:19:27 +0530

    Made flintbit readable and workable

--- restart_httpd.rb
+++ restart_httpd.rb
@@ -1,4 +1,7 @@
+#Log all input at call 
 @log.info("input is:"+ @input.to_s)
+
+# Initialization 
 
 servicename=               @input.get("servicename")   #getting the values from JSON
 hostname=                  @input.get("hostname")
@@ -17,107 +20,121 @@ manageenginerequestid=     @input.get("MANAGE_ENGINE_REQUESTID")
 manageenginesubject=	   @input.get("MANAGE_ENGINE_REQUESTSUB")
 alerttype=		   @input.get("ALERTTYPE")
 
+# Initialization  ends here
+
+#We call Manage Engine Updating the ticket with intent to auto resolve
 @log.info("restart_httpd was called for host "+ hostname +"Related incident Ticket Number : "+ manageenginerequestid )
 
-   response=@call.connector("manageenginesdp")    
-              .set("action","update-request")
-              .set("request-id",manageenginerequestid.to_i)
-              .set("requester","Flint Operator")
-              .set("subject",manageenginesubject.to_s)
-              .set("description", servicedesc)
-              .set("requesttemplate","Unable to browse")
-              .set("priority","Low")
-              .set("site","-")
-              .set("group","Network")
-              .set("technician","Flint Operator")
-              .set("level","Tier 3")
-              .set("status","Close")
-              .set("service",@service)
-              .timeout(10000)                                                 
-              .async
+  response=@call.connector("manageenginesdp")    
+           .set("action","update-request")
+           .set("request-id",manageenginerequestid.to_i)
+           .set("requester","Flint Operator")
+           .set("subject",manageenginesubject.to_s)
+           .set("description", servicedesc)
+           .set("requesttemplate","Unable to browse")
+           .set("priority","Low")
+           .set("site","-")
+           .set("group","Network")
+           .set("technician","Flint Operator")
+           .set("level","Tier 3")
+           .set("status","Close")
+           .set("service",@service)
+           .timeout(10000)                                                 
+           .async
     
-     result=response.get("result")
-     @log.info("#{result.to_s}")
+  result=response.get("result")
+  @log.info("#{result.to_s}")
 
-if  alerttype == "HTTP"                                       #service goes ‘Down’
-  response1=@call.connector("ssh")                                   #calling ssh connector   
-	.set("target",hostaddress)
-	.set("type","exec")             
-	.set("username","root")
-	.set("password","Flint@01")
-	.set("command","systemctl -q restart httpd.service && systemctl is-active httpd && logout")     #Starting web server apache2
-	.set("timeout",60000)
-	.sync
+# Manage Engine  Call ends here 
 
-  result1=response1.get("result")
-  @log.info("#{result1.to_s}")
+#Auto resolve on basis of alertype  Code starts here 
+
+#Call to auto resolve based on alrt type HTTP 
+
+  if  alerttype == "HTTP"                                       #service goes ‘Down’
+	response1=@call.connector("ssh")                                   #calling ssh connector   
+		  .set("target",hostaddress)
+		  .set("type","exec")             
+		  .set("username","root")
+		  .set("password","Flint@01")
+		  .set("command","systemctl -q restart httpd.service && systemctl is-active httpd && logout")     #Starting web server apache2
+		  .set("timeout",60000)
+		  .sync
+
+        result1=response1.get("result")
+        @log.info("#{result1.to_s}")
 
 
 
 	  # closing request 
 	response2=@call.connector("manageenginesdp")    
-              .set("action","close-request")
-              .set("request-id",manageenginerequestid.to_i)
-              .set("close-accepted","Accepted")
-              .set("close-comment","Service restarted successfully")                               
-              .async
+                  .set("action","close-request")
+                  .set("request-id",manageenginerequestid.to_i)
+                  .set("close-accepted","Accepted")
+                  .set("close-comment","Service restarted successfully")                               
+                  .async
 
 
-    result2=response2.get("result")
-    @log.info("#{result2.to_s}")
+        result2=response2.get("result")
+        @log.info("#{result2.to_s}")
 
-end
+  end
 
-if  alerttype == "DISK"                                       #service goes ‘Down’
-  response3=@call.connector("ssh")                                   #calling ssh connector   
-	.set("target",hostaddress)
-	.set("type","exec")             
-	.set("username","root")
-	.set("password","Flint@01")
-	.set("command","lvextend -L+5M /dev/mapper/flintvg-flint_vol1 1>/dev/zero 2>/dev/zero  &&  resize2fs /dev/mapper/flintvg-flint_vol1 1>/dev/zero 2>/dev/zero ; echo $?")     
-	.set("timeout",60000)
-	.sync
+#Call to auto resolve for alert type  HTTP ends here 
 
-  #SSH Connector Response Parameter
-  result3=response3.get("result")
-  @log.info("#{result3.to_s}")
+#Call to auto resolve based on alert type DISK 
+
+  if  alerttype == "DISK" 
+	response3=@call.connector("ssh")                                   #calling ssh connector   
+		  .set("target",hostaddress)
+		  .set("type","exec")             
+		  .set("username","root")
+		  .set("password","Flint@01")
+		  .set("command","lvextend -L+5M /dev/mapper/flintvg-flint_vol1 1>/dev/zero 2>/dev/zero  &&  resize2fs /dev/mapper/flintvg-flint_vol1 1>/dev/zero 2>/dev/zero ; echo $?")     
+		  .set("timeout",60000)
+		  .sync
+
+        result3=response3.get("result")
+        @log.info("#{result3.to_s}")
 
 
 
-    if result3.include? "Insufficient"
-	 @log.info("SSH command to resize/extend VG/FS failed") 
-	 response5 = @call.connector('manageenginesdp')
-                .set('action','add-request')
-                .set('requester','Flint-bit Automation code')
-                .set('subject', 'Add request')
-                .set('description', 'Requesting LUNS from Storage Admins :Not enough LUNS to grow VG ,Auto Resolution fails Refer to acted alert '+ manageenginerequestid)
-                .set('requesttemplate', 'Unable to browse')
-                .set('requestType','Incident')
-                .set('priority', 'High')
-                .set('site', '-')
-                .set('group','Network')
-                .set('technician', 'John')
-                .set('level', 'Tier 1')
-                .set('status', 'Open')
-                .set('service', 'Hardware')
-                .timeout(10000)
-                .sync
+	if result3.include? "Insufficient"
+	@log.info("SSH command to resize/extend VG/FS failed") 
 
-	result5=response5.get("result")
+	response4 = @call.connector('manageenginesdp')
+                   .set('action','add-request')
+                   .set('requester','Flint-bit Automation code')
+                   .set('subject', 'Add request')
+                   .set('description', 'Requesting LUNS from Storage Admins :Not enough LUNS to grow VG ,Auto Resolution fails Refer to acted alert '+ manageenginerequestid)
+                   .set('requesttemplate', 'Unable to browse')
+                   .set('requestType','Incident')
+                   .set('priority', 'High')
+                   .set('site', '-')
+                   .set('group','Network')
+                   .set('technician', 'John')
+                   .set('level', 'Tier 1')
+                   .set('status', 'Open')
+                   .set('service', 'Hardware')
+                   .timeout(10000)
+                   .sync
+
+	result4=response4.get("result")
 	@log.info("#{result5.to_s}")
 	
-     else   
-	# closing request
-	response4=@call.connector("manageenginesdp")    
-              .set("action","close-request")
-              .set("request-id",manageenginerequestid.to_i)
-              .set("close-accepted","Accepted")
-              .set("close-comment","Volume Group expanded successfully")                               
-              .async
+        else   
+	
+	  response5=@call.connector("manageenginesdp")    
+                    .set("action","close-request")
+                    .set("request-id",manageenginerequestid.to_i)
+                    .set("close-accepted","Accepted")
+                    .set("close-comment","Volume Group expanded successfully")                               
+                    .sync
 
-        result4=response4.get("result")
-	@log.info("#{result4.to_s}")
+          result5=response5.get("result")
+	  @log.info("#{result4.to_s}")
 
-    end
+        end
 
-end
+
+  end
