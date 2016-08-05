5ce194a6261283377f69c70f6a2db932f27dea1d
sgopalakrishnan1980 <gopal.subramanian@accenture.com>
2016-08-05 17:15:28 +0530

    Updated version of notify_flint.sh

--- notify_flint.sh
+++ notify_flint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash 
 #
 ARGS="$*"
 HOSTATTEMPT=$(echo $ARGS | awk -F '-EOA-' '{printf $1}')
@@ -16,12 +16,19 @@ SERVICEEVENTID=$(echo $ARGS | awk -F '-EOA-' '{printf $12}')
 HOSTSTATETYPE=$(echo $ARGS | awk -F '-EOA-' '{printf $13}')
 PROCESS_ID="$$"
 
+ISALERTTYPE_DISK=$(echo $ARGS | awk -F '-EOA-' '{printf $11}' | grep -q -i Disk ; echo $?)
+if [[ $ISALERTTYPE_DISK -eq 0 ]];
+   then ALERT_TYPE="DISK"
+else 
+   ALERT_TYPE="HTTP"
+fi
 
 TECHNICIAN_KEY="950BF8C4-4BB0-41D0-A893-E3D69BEEE7AE"
 
 
 #create xml for Manage Engine Service Desk 
 # Note remove all whitespace in payload . to Retain white space use %20
+MESDP_SUBJECT="Issues detected with $HOSTADDRESS :  $SERVICEDISPLAYNAME reported $SERVICESTATE"
 
 MESDP_PAYLOAD="<Operation>\
 <Details>\
@@ -42,7 +49,6 @@ MESDP_PAYLOAD="<Operation>\
 
 
 call_managesdp(){
-set -x
 #call Manage Engine Retrieve workoerder id for flint
 /usr/bin/curl --connect-timeout 60 -X POST \
   -H "Content-Type:x-www-form-urlencoded"\
@@ -65,7 +71,7 @@ fi
 call_flint(){
 #call Flint 
 
-DATA="{\"servicename\":\""$SERVICEDISPLAYNAME"\",\"servicestate\":\""$SERVICESTATE"\",\"hostname\":\""$HOSTNAME"\",\"hoststatetype\":\""$HOSTSTATETYPE"\",\"hostattempt\":\""$HOSTATTEMPT"\",\"servicedesc\":\""$SERVICEDESC"\",\"servicestateid\":\""$SERVICESTATEID"\",\"serviceeventid\":\""$SERVICEEVENTID"\",\"serviceproblemid\":\""$SERVICEPROBLEMID"\",\"servicelatency\":\""$SERVICELATENCY"\",\"serviceexecutiontime\":\""$SERVICEEXECUTIONTIME"\",\"serviceduration\":\""$SERVICEDURATION"\",\"MANAGE_ENGINE_REQUESTID\":\""$MANAGE_ENGINE_REQUESTID"\",\"hostaddress\":\""$HOSTADDRESS"\"}"
+DATA="{\"servicename\":\""$SERVICEDISPLAYNAME"\",\"servicestate\":\""$SERVICESTATE"\",\"hostname\":\""$HOSTNAME"\",\"hoststatetype\":\""$HOSTSTATETYPE"\",\"hostattempt\":\""$HOSTATTEMPT"\",\"servicedesc\":\""$SERVICEDESC"\",\"servicestateid\":\""$SERVICESTATEID"\",\"serviceeventid\":\""$SERVICEEVENTID"\",\"serviceproblemid\":\""$SERVICEPROBLEMID"\",\"servicelatency\":\""$SERVICELATENCY"\",\"serviceexecutiontime\":\""$SERVICEEXECUTIONTIME"\",\"serviceduration\":\""$SERVICEDURATION"\",\"MANAGE_ENGINE_REQUESTID\":\""$MANAGE_ENGINE_REQUESTID"\",\"MANAGE_ENGINE_REQUESTSUB\":\""$MESDP_SUBJECT"\",\"ALERTTYPE\":\""$ALERT_TYPE"\",\"hostaddress\":\""$HOSTADDRESS"\"}"
 
 echo "$(date "+%d-%m-%y %T") INFO PID $PROCESS_ID DATA = $DATA "  >> /var/log/nagios/eventnotifier.log
 
@@ -74,7 +80,6 @@ echo "$(date "+%d-%m-%y %T") INFO PID $PROCESS_ID DATA = $DATA "  >> /var/log/na
 -H "x-flint-username: admin" \
 -H "x-flint-password: admin123" \
 -d "$DATA" 'http://192.168.1.200:3501/v1/bit/run/httpdrecipies:restart_httpd.rb'  -o  /var/log/nagios_notify_flint.log
-#-d "'"{"servicename":"$SERVICEDISPLAYNAME","servicestate": "$SERVICESTATE", "hostname":"$HOSTNAME","hoststatetype":"$HOSTSTATETYPE","hostattempt":"$HOSTATTEMPT", "servicedesc": "$SERVICEDESC", "servicestateid":"$SERVICESTATEID","serviceeventid":"$SERVICEEVENTID","serviceproblemid":"$SERVICEPROBLEMID","servicelatency":"$SERVICELATENCY","serviceexecutiontime":"$SERVICEEXECUTIONTIME","serviceduration":"$SERVICEDURATION","hostaddress":"$HOSTADDRESS"}"'" 'http://192.168.1.200:3501/v1/bit/run/httpdrecipies:restart_httpd.rb'  -o  /var/log/nagios_notify_flint.log
 
 if [[ $? -eq 0 ]];
    then echo "$(date "+%d-%m-%y %T") INFO PID $PROCESS_ID Call to Flint Box  Succeeded "  >> /var/log/nagios/eventnotifier.log
@@ -97,7 +102,7 @@ echo "$(date "+%d-%m-%y %T") INFO PID $PROCESS_ID json created as follows:\n$DAT
 	then echo "$(date "+%d-%m-%y %T") INFO PID $PROCESS_ID event notifier invoked from Monitoring for $HOSTADDRESS  Alert Type : $SERVICESTATE " >> /var/log/nagios/eventnotifier.log
 	call_managesdp 
         if [[ $? -eq 0 ]];
-           then call_flint
+           then sleep 10 && call_flint
         else
  	   echo " Call to call_managesdp failed " >> /var/log/nagios/eventnotifier.log 
 	fi
